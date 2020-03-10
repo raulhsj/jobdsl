91fb677f0f17ff2a9df97b8926b4f0324a89e1d6
Raúl Hernández <raul.hernandez@ust-global.com>
2020-03-10 09:27:49 +0100

    wip

--- jobs/multibranch.groovy
+++ jobs/multibranch.groovy
@@ -1,9 +1,11 @@
+final String gitId = "123456789"
 multibranchPipelineJob('node-deploy') {
     triggers {
-      periodic(1)
+      cron('* * * * *')
     }
     branchSources {
         git {
+            id(gitId) / IMPORTANT: use a constant and unique identifier
             remote('git@github.com:raulhsj/tests-jenkins-1.git')
             credentialsId('cadc4801-ca68-4fdd-8aa7-46fcd9e4b976')
         }
