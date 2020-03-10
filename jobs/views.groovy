3207a57d2008ee35f78033105e596da51718ea2e
Raúl Hernández <raul.hernandez@ust-global.com>
2020-03-10 08:54:56 +0100

    wip

--- jobs/views.groovy
+++ jobs/views.groovy
@@ -2,5 +2,14 @@ listView('node') {
     description('node pipelines')
     jobs {
         regex(/.*test.*/)
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
     }    
 }
